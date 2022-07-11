436cc494feb9c89b04f8f154ca4c5d3fa29689aa
Zavodovski <lqhheart@gmail.com>
2022-07-11 11:41:28 +0800

    Fixed Function Pipeline Stages

--- Little_Vulkan_Engine/Lve_pipeline.hpp
+++ Little_Vulkan_Engine/Lve_pipeline.hpp
@@ -7,7 +7,29 @@
 namespace lve
 {
 
-    struct PipelineConfigInfo {};
+    struct PipelineConfigInfo 
+    {
+        //PipelineConfigInfo() = default;
+        //PipelineConfigInfo(const PipelineConfigInfo&) = delete;
+        //PipelineConfigInfo& operator=(const PipelineConfigInfo&) = delete;
+
+        VkViewport viewport;
+        VkRect2D scissor;
+        //std::vector<VkVertexInputBindingDescription> bindingDescriptions{};
+        //std::vector<VkVertexInputAttributeDescription> attributeDescriptions{};
+        VkPipelineViewportStateCreateInfo viewportInfo;
+        VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+        VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+        VkPipelineMultisampleStateCreateInfo multisampleInfo;
+        VkPipelineColorBlendAttachmentState colorBlendAttachment;
+        VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+        VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+        //std::vector<VkDynamicState> dynamicStateEnables;
+        //VkPipelineDynamicStateCreateInfo dynamicStateInfo;
+        VkPipelineLayout pipelineLayout = nullptr;
+        VkRenderPass renderPass = nullptr;
+        uint32_t subpass = 0;
+    };
 
     class Lvepipeline
     {
@@ -16,7 +38,7 @@ namespace lve
                     const std::string& vertFilepath, 
                     const std::string& fragFilepath,
                     const PipelineConfigInfo& configInfo);
-        ~Lvepipeline() {}
+        ~Lvepipeline();
 
         Lvepipeline(const Lvepipeline&) = delete;
         Lvepipeline& operator=(const Lvepipeline&) = delete;
